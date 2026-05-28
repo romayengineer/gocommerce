6918076e792343eaf0db8310c55c9aba608dd78f
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-05-28 18:39:15 -0300

    for windows

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [sveltekit()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 });
