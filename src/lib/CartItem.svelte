11db2ab80567d41d2d2f86393a0f7dd7c962271d
Maximiliano Romay Figueroa <maximiliano.figueroa@valtira.net>
2026-06-06 23:45:53 -0300

    fix button color and padding in cart

--- src/lib/CartItem.svelte
+++ src/lib/CartItem.svelte
@@ -16,8 +16,8 @@
 </script>
 
 <div class="shadow border border-1 border-grey-100">
-	<Button class="bg-white text-red-600 px-2 py-1 float-right" onclick={onRemove}>
-		<Trash2 size={20} />
+	<Button class="bg-white text-red-600px-2 py-1 float-right hover:bg-white" onclick={onRemove}>
+		<Trash2 size={20} class="bg-white text-red-600"/>
 	</Button>
 	<div class="grid grid-cols-[auto_1fr] py-4 px-2">
 		{#if item.product.images.length > 0}
@@ -25,7 +25,7 @@
 				<img src={item.product.images[0]} alt={item.product.nameComplete} class="w-full h-full object-cover rounded" />
 			</div>
 		{/if}
-		<div class="flex flex-col">
+		<div class="flex flex-col pl-4">
 			<h3 class="font-semibold text-lg pb-2 flex-1">{item.product.nameComplete}</h3>
 
 			<div class="flex items-center gap-4">
