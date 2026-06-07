11a439c685e3b604a4cb46cc08adb83ac6b4d143
Maximiliano Romay Figueroa <maximiliano.figueroa@valtira.net>
2026-06-06 23:07:25 -0300

    nice cart

--- src/lib/CartItem.svelte
+++ src/lib/CartItem.svelte
@@ -17,14 +17,14 @@
 	<Button class="bg-white text-2xl text-red-600 px-3 py-1 float-right" onclick={onRemove}>
 		x
 	</Button>
-	<div class="grid grid-cols-[auto_1fr] py-4">
+	<div class="grid grid-cols-[auto_1fr] py-4 px-2">
 		{#if item.product.images.length > 0}
-			<div class="w-20 h-30">
+			<div class="w-16 h-30">
 				<img src={item.product.images[0]} alt={item.product.nameComplete} class="w-full h-full object-cover rounded" />
 			</div>
 		{/if}
-		<div>
-			<h3 class="font-semibold text-lg pb-2">{item.product.nameComplete}</h3>
+		<div class="flex flex-col">
+			<h3 class="font-semibold text-lg pb-2 flex-1">{item.product.nameComplete}</h3>
 
 			<div class="flex items-center gap-4">
 				<QuantitySelector quantity={item.quantity} onchange={onQuantityChange} />
