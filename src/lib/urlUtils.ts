6c00a2a23a42881072e4a7a76e283739816797b6
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-06-17 09:48:07 -0300

    we only use hash routing

--- src/lib/urlUtils.ts
+++ src/lib/urlUtils.ts
@@ -1,24 +1,22 @@
 export function updatePageInUrl(url: string, page: number): string {
-	const hashIndex = url.indexOf('#');
+	let hashIndex = url.indexOf('#');
+	if (hashIndex < 0) {
+		url = `${url}#/`
+	}
+	hashIndex = url.indexOf('#');
 
-	if (hashIndex > -1) {
-		const beforeHash = url.substring(0, hashIndex);
-		const hash = url.substring(hashIndex);
-		const queryIndex = hash.indexOf('?');
+	const beforeHash = url.substring(0, hashIndex);
+	const hash = url.substring(hashIndex);
+	const queryIndex = hash.indexOf('?');
 
-		if (queryIndex > -1) {
-			const hashPath = hash.substring(0, queryIndex);
-			const queryString = hash.substring(queryIndex + 1);
-			const params = new URLSearchParams(queryString);
-			params.set('page', String(page));
-			return beforeHash + hashPath + '?' + params.toString();
-		} else {
-			return beforeHash + hash + '?page=' + page;
-		}
+	if (queryIndex > -1) {
+		const hashPath = hash.substring(0, queryIndex);
+		const queryString = hash.substring(queryIndex + 1);
+		const params = new URLSearchParams(queryString);
+		params.set('page', String(page));
+		return beforeHash + hashPath + '?' + params.toString();
 	} else {
-		const urlObj = new URL(url);
-		urlObj.searchParams.set('page', String(page));
-		return urlObj.toString();
+		return beforeHash + hash + '?page=' + page;
 	}
 }
 
@@ -35,9 +33,7 @@ export function getPageInUrl(url: string): number {
 			return page ? parseInt(page) : 1;
 		}
 		return 1;
-	} else {
-		const urlObj = new URL(url);
-		const page = urlObj.searchParams.get('page');
-		return page ? parseInt(page) : 1;
 	}
+
+	return 1;
 }
