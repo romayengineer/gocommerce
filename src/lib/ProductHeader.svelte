caa421e7771b3bbc7bdfbea6ecd6c8874a6272b3
Maximiliano Romay Figueroa <maximiliano.figueroa@valtira.net>
2026-06-21 11:50:16 -0300

    brand and title together

--- src/lib/ProductHeader.svelte
+++ src/lib/ProductHeader.svelte
@@ -13,12 +13,9 @@
 </script>
 
 <div>
-	<h1 class="capitalize x-text-2xl font-bold mb-4">{product.nameComplete}</h1>
-	<div class="mb-4">
-		<SizeSelector items={product.items} selected={itemSelected} onSelect={selectSize} />
-	</div>
+	<h1 class="capitalize x-text-2xl font-bold mb-4">{product.brand} {product.nameComplete}</h1>
 	<div class="mb-4 flex flex-wrap justify-between gap-4">
-		<span class="capitalize text-xl text-gray-600">{product.brand}</span>
+		<SizeSelector items={product.items} selected={itemSelected} onSelect={selectSize} />
 		<Price amount={product.items[itemSelected].price} size="lg" />
 	</div>
 </div>
